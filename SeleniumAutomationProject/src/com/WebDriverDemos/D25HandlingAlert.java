c1e2de51d3ed45dde3306af2a06f92b02758cda9
Ankush <ankushvankore@gmail.com>
2024-09-26 22:38:49 +0530

    Date: 26-09-2024

---
+++ SeleniumAutomationProject/src/com/WebDriverDemos/D25HandlingAlert.java
@@ -0,0 +1,42 @@
+package com.WebDriverDemos;
+
+import java.time.Duration;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class D25HandlingAlert {
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+		driver.get("https://demoqa.com/alerts");
+		
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		Alert alt;
+		
+		js.executeScript("arguments[0].click()", driver.findElement(By.id("alertButton")));
+		//driver.findElement(By.id("alertButton")).click();
+		alt = driver.switchTo().alert();
+		System.out.println(alt.getText());
+		alt.accept();				//Will click on Ok button
+		
+		js.executeScript("arguments[0].click()", driver.findElement(By.id("timerAlertButton")));
+		//Thread.sleep(5000);
+		wait.until(ExpectedConditions.alertIsPresent());
+		alt = driver.switchTo().alert();
+		System.out.println(alt.getText());
+		alt.accept();
+		
+		
+	}
+
+}
